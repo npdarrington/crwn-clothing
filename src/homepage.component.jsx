bfb7218a1c9be930414c0a27afadfdb195265d61
Nathan Darrington <npdarrington@icloud.com>
2020-09-29 17:47:34 -0600

    Create homepage.component.jsx and added basic homepage structure

---
+++ src/homepage.component.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const HomePage = () => (
+  <section className="homepage">
+    <section className="directory-menu">
+      <article className="menu-item">
+        <section className="content">
+          <h1 className="title">Hats</h1>
+          <span className="title">SPAN NOW</span>
+        </section>
+        <section className="content">
+          <h1 className="title">Jackets</h1>
+          <span className="title">SPAN NOW</span>
+        </section>
+        <section className="content">
+          <h1 className="title">Sneakers</h1>
+          <span className="title">SPAN NOW</span>
+        </section>
+        <section className="content">
+          <h1 className="title">Womens</h1>
+          <span className="title">SPAN NOW</span>
+        </section>
+        <section className="content">
+          <h1 className="title">Mens</h1>
+          <span className="title">SPAN NOW</span>
+        </section>
+      </article>
+    </section>
+  </section>
+);
+
+export default HomePage;
