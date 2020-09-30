f1b9cab8b65364a99a0ec8469eddbaa5cedf49d1
Nathan Darrington <npdarrington@icloud.com>
2020-09-29 19:26:36 -0600

    Changed background to its own section to add transition effect and toUpperCase title

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import './menu-item.styles.scss';
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <article style={{ 
-      backgroundImage: `url(${imageUrl})`
-    }}
-    className={`${size} menu-item`}
-  >
+  <article className={`${size} menu-item`}>
+    <section className="background-image" 
+      style={{ 
+        backgroundImage: `url(${imageUrl})`
+      }}>
+    </section>
     <section className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </section>
   </article>
