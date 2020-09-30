88cca90a29f5d75bdbc54f973abbc17c5104b9b2
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 11:33:55 -0600

    Modified App from a PC to a state class that auths user with firebase on componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
@@ -7,18 +7,40 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
+import { auth } from './firebase/firebase.utils';
 
-function App() {
-	return (
-		<section className='App'>
-			<Header />
-			<Switch>
-				<Route exact path='/' component={HomePage} />
-				<Route exact path='/shop' component={ShopPage} />
-				<Route exact path='/signin' component={SignInAndSignUpPage} />
-			</Switch>
-		</section>
-	);
+export default class App extends Component {
+	constructor() {
+		super();
+
+		this.state = {
+			currentUser: null,
+		};
+	}
+
+	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+			this.setState({ currentUser: user });
+			console.log(user);
+		});
+	}
+
+	componentWillUnmount() {
+		this.unsubscribeFromAuth();
+	}
+
+	render() {
+		return (
+			<section className='App'>
+				<Header />
+				<Switch>
+					<Route exact path='/' component={HomePage} />
+					<Route exact path='/shop' component={ShopPage} />
+					<Route exact path='/signin' component={SignInAndSignUpPage} />
+				</Switch>
+			</section>
+		);
+	}
 }
-
-export default App;
