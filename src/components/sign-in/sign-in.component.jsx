dd4037d0242ea35e48b51b04f01242f42d614780
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 11:22:04 -0600

    Imported signInWithGoogle method and created another CustomButton to sign in with google oAuth

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 import './sign-in.styles.scss';
 
 export default class SignIn extends Component {
@@ -53,6 +55,7 @@ export default class SignIn extends Component {
 						required
 					/>
 					<CustomButton type='submit'>Sign In</CustomButton>
+					<CustomButton onClick={signInWithGoogle}>{' '}Sign in with Google{' '}</CustomButton>
 				</form>
 			</article>
 		);
