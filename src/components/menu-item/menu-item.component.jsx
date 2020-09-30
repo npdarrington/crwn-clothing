9c6472224f5ead8ef173c83c9a03dcef8a3b56e5
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 00:01:52 -0600

    Changed PC MenuItem to accept linkUrl and match for routing as well as export withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
 
 import './menu-item.styles.scss';
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <article className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <article className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
     <section className="background-image" 
       style={{ 
         backgroundImage: `url(${imageUrl})`
@@ -17,4 +18,4 @@ const MenuItem = ({ title, imageUrl, size }) => (
 )
 
 
-export default MenuItem
+export default withRouter(MenuItem);
