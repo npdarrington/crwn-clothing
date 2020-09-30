f081613bc8b0a0b964863134c04b95a7f80d19b4
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 00:36:59 -0600

    Added missing () on toUpperCase and modified map to spread in otherItemProps

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 
+import CollectionItem from '../collection-item/collection-item.component';
+
 import './collection-preview.styles.scss';
 
 const CollectionPreview = ({ title, items }) => (
 	<section className='collection-preview'>
-		<h1 className='title'>{title.toUpperCase}</h1>
+		<h1 className='title'>{title.toUpperCase()}</h1>
 		<article className='preview'>
 			{items
 				.filter((item, index) => index < 4)
-				.map(item => (
-					<article key={item.id}>{item.name}</article>
+				.map(({ id, ...otherItemProps }) => (
+					<CollectionItem key={id} {...otherItemProps} />
 				))}
 		</article>
 	</section>
