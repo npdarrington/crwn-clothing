7737d6e19cb24ec5dd2caa6c194ab8c920b8bd42
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 00:00:51 -0600

    Added Switch and Route with react-router-dom and testing with HatsPage function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 
+const HatsPage = () => (
+	<section>
+		<h1>HATS PAGE</h1>
+	</section>
+);
+
 function App() {
-  return (
-    <section className="App">
-      <HomePage />
-    </section>
-  );
+	return (
+		<section className='App'>
+			<Switch>
+				<Route exact path='/' component={HomePage} />
+				<Route exact path='/hats' component={HatsPage} />
+			</Switch>
+		</section>
+	);
 }
 
 export default App;
