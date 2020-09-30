72c32048642c33fec9c68c2d92d9ddc0133175a2
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 00:20:25 -0600

    Formatted code

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,28 +1,26 @@
 import React, { Component } from 'react';
 
-import SHOP_DATA from './shop.data'
+import SHOP_DATA from './shop.data';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component'
+import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
 export default class ShopPage extends Component {
-  constructor(props) {
-    super(props);
+	constructor(props) {
+		super(props);
 
-    this.state = {
-      collections: SHOP_DATA
-    }
-  }
+		this.state = {
+			collections: SHOP_DATA,
+		};
+	}
 
-  render() {
-    const {collections} = this.state;
-    return (
-      <section className='shop-page'>
-        {
-          collections.map(({ id, ...otherCollectionProps }) => (
-            <CollectionPreview key={id} {...otherCollectionProps} />
-          ))
-        }
-      </section>
-    )
-  }
+	render() {
+		const { collections } = this.state;
+		return (
+			<section className='shop-page'>
+				{collections.map(({ id, ...otherCollectionProps }) => (
+					<CollectionPreview key={id} {...otherCollectionProps} />
+				))}
+			</section>
+		);
+	}
 }
