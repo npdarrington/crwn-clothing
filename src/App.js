7529f78911baae32b509500a18f33ae8c7dd7014
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 00:18:53 -0600

    Deleted test HatsPage function and imported new ShopPage component

--- src/App.js
+++ src/App.js
@@ -4,19 +4,14 @@ import { Switch, Route } from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
-
-const HatsPage = () => (
-	<section>
-		<h1>HATS PAGE</h1>
-	</section>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
 	return (
 		<section className='App'>
 			<Switch>
 				<Route exact path='/' component={HomePage} />
-				<Route exact path='/hats' component={HatsPage} />
+				<Route exact path='/shop' component={ShopPage} />
 			</Switch>
 		</section>
 	);
