fb70a2df5bfecacc1ff1318c453922ef669a6839
Nathan Darrington <npdarrington@icloud.com>
2020-09-30 11:11:37 -0600

    Added sign-in-and-sign-up page

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component'
+
+import './sign-in-and-sign-up.styles.scss';
+
+const SignInAndSignUpPage = () => (
+	<section className='sign-in-and-sign-up'>
+    <SignIn />
+  </section>
+);
+
+export default SignInAndSignUpPage;
