327b1859a77b5d7875c5e91a9b536553bb6c058d
Nathan Darrington <npdarrington@icloud.com>
2020-09-29 19:00:47 -0600

    Transitioned top lvl dir homepage.component and homepage.styles into their own dir inside components pages

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../components/directory/directory.component'
+
+import './homepage.styles.scss'
+
+const HomePage = () => (
+  <section className="homepage">
+    <Directory />
+  </section>
+);
+
+export default HomePage;
