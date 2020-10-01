4e3c669d6a0d19ad6d17c45021c6cefb6567e7cd
Nathan Darrington <npdarrington@icloud.com>
2020-10-01 09:19:56 -0600

    Added imports for sign-in-component and created state component

---
+++ src/components/sign-up/sign-up.component.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
+
+import { auth, createUserProfileDocument } from '../../firebase/firebase.utils';
+
+import './sign-up.styles.scss';
+
+export default class SignUp extends Component {
+	
+}
