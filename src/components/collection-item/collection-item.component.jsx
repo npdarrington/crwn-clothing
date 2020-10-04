611e8db0f43cb7d5e270a2b8c4d27e9f986201c2
Nathan Darrington <npdarrington@icloud.com>
2020-10-04 11:31:12 -0600

    Changed CollectionItem into HCO with connect and changed to destructure entire item to pass addItem type from cart redux

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-import CustomButton from '../custom-button/custom-button.component'
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
 
-const CollectionItem = ({ id, name, price, imageUrl }) => (
-	<section className='collection-item'>
-		<section
-			className='image'
-			style={{
-				backgroundImage: `url(${imageUrl})`,
-			}}
-		></section>
-		<section className='collection-footer'>
-			<span className='name'>{name}</span>
-			<span className='price'>{price}</span>
+const CollectionItem = ({ item, addItem }) => {
+	const { name, price, imageUrl } = item;
+	return (
+		<section className='collection-item'>
+			<section
+				className='image'
+				style={{
+					backgroundImage: `url(${imageUrl})`,
+				}}
+			></section>
+			<section className='collection-footer'>
+				<span className='name'>{name}</span>
+				<span className='price'>{price}</span>
+			</section>
+			<CustomButton onClick={() => addItem(item)} inverted>
+				Add to Cart
+			</CustomButton>
 		</section>
-		<CustomButton inverted>Add to Cart</CustomButton>
-	</section>
-);
+	);
+};
 
-export default CollectionItem;
+const mapDispatchToProps = dispatch => ({
+	addItem: item => dispatch(addItem(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CollectionItem);
