4b35612ffc86bd1380a6789cba7e3fb9a65e756e
Nathan Darrington <npdarrington@icloud.com>
2020-10-05 00:42:08 -0600

    Changed checkout.component to a HOC that will use createStructureSelector to get total and cartItems

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -1,7 +1,43 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
+import {
+	selectCartItems,
+	selectCartTotal,
+} from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
 
-const CheckoutPage = () => <section>CHECKOUT PAGE</section>;
+const CheckoutPage = ({ cartItems, total }) => (
+	<section className='checkout-page'>
+		<section className='checkout-header'>
+			<article className='header-block'>
+				<span>Product</span>
+			</article>
+			<article className='header-block'>
+				<span>Description</span>
+			</article>
+			<article className='header-block'>
+				<span>Quantity</span>
+			</article>
+			<article className='header-block'>
+				<span>Price</span>
+			</article>
+			<article className='header-block'>
+				<span>Remove</span>
+			</article>
+		</section>
+		{cartItems.map(cartItem => cartItem.name)}
+		<article className='total'>
+			<span>TOTAL: ${total}</span>
+		</article>
+	</section>
+);
 
-export default CheckoutPage;
+const mapStateToProps = createStructuredSelector({
+	total: selectCartTotal,
+	cartItems: selectCartItems,
+});
+
+export default connect(mapStateToProps)(CheckoutPage);
