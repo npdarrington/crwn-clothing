4364e39bc3b0caadf7fbe64b16d974c2dbd4141e
Nathan Darrington <npdarrington@icloud.com>
2020-10-05 01:15:45 -0600

    Modified CheckoutItem to allow for clearItemFromCart from redux to work, used connect to turn CheckoutItem into HOC

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => (
-	<article className='checkout-item'>
-		<section className='image-container'>
-			<img src={imageUrl} alt='item' />
-		</section>
-		<span className='name'>{name}</span>
-		<span className='quantity'>{quantity}</span>
-		<span className='price'>{price}</span>
-		<section className='remove-button'>&#10005;</section>
-	</article>
-);
+const CheckoutItem = ({ cartItem, clearItem }) => {
+	const { name, imageUrl, price, quantity } = cartItem;
+	return (
+		<article className='checkout-item'>
+			<section className='image-container'>
+				<img src={imageUrl} alt='item' />
+			</section>
+			<span className='name'>{name}</span>
+			<span className='quantity'>{quantity}</span>
+			<span className='price'>{price}</span>
+			<section className='remove-button' onClick={() => clearItem(cartItem)}>
+				&#10005;
+			</section>
+		</article>
+	);
+};
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+	clearItem: item => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
