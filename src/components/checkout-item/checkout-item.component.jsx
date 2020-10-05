52900d189ced246c9087d344fa68e032af468159
Nathan Darrington <npdarrington@icloud.com>
2020-10-05 01:51:15 -0600

    Added left and right arrow around quantity where a user can add, decrease and remove items inside CheckoutPage

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
 
-import { clearItemFromCart } from '../../redux/cart/cart.actions';
+import {
+	clearItemFromCart,
+	addItem,
+	removeItem,
+} from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ cartItem, clearItem }) => {
+const CheckoutItem = ({ cartItem, clearItem, addItem, removeItem }) => {
 	const { name, imageUrl, price, quantity } = cartItem;
 	return (
 		<article className='checkout-item'>
@@ -13,7 +17,11 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 				<img src={imageUrl} alt='item' />
 			</section>
 			<span className='name'>{name}</span>
-			<span className='quantity'>{quantity}</span>
+			<span className='quantity'>
+				<article className='arrow' onClick={() => removeItem(cartItem)}>&#10094;</article>
+				<span className='value'>{quantity}</span>
+				<article className='arrow' onClick={() => addItem(cartItem)}>&#10095;</article>
+			</span>
 			<span className='price'>{price}</span>
 			<section className='remove-button' onClick={() => clearItem(cartItem)}>
 				&#10005;
@@ -24,6 +32,8 @@ const CheckoutItem = ({ cartItem, clearItem }) => {
 
 const mapDispatchToProps = dispatch => ({
 	clearItem: item => dispatch(clearItemFromCart(item)),
+	addItem: item => dispatch(addItem(item)),
+	removeItem: item => dispatch(removeItem(item)),
 });
 
 export default connect(null, mapDispatchToProps)(CheckoutItem);
