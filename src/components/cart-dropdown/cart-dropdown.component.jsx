39cecbb22fc119bb3d9284cf268e52dbdc9dc33f
Nathan Darrington <npdarrington@icloud.com>
2020-10-04 22:34:17 -0600

    Changed CartDropDown into a HOC passing in cartItems from state to show items added in CartDropDown

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,14 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
-const CartDropdown = () => (
+const CartDropdown = ({ cartItems }) => (
 	<article className='cart-dropdown'>
-		<section className='cart-items'></section>
+		<section className='cart-items'>
+			{cartItems.map(cartItem => (
+				<CartItem key={cartItem.id} item={cartItem} />
+			))}
+		</section>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</article>
 );
 
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+	cartItems,
+});
+
+export default connect(mapStateToProps)(CartDropdown);
