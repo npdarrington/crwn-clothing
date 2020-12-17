b067e454769df297ee182b8be88f29e5324981a9
Nathan Darrington <npdarrington@icloud.com>
2020-12-16 21:52:22 -0700

    Reformat code

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import SignIn from '../../components/sign-in/sign-in.component'
-import SignUp from '../../components/sign-up/sign-up.component'
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './sign-in-and-sign-up.styles.scss';
 
 const SignInAndSignUpPage = () => (
 	<section className='sign-in-and-sign-up'>
-    <SignIn />
-    <SignUp />
-  </section>
+		<SignIn />
+		<SignUp />
+	</section>
 );
 
 export default SignInAndSignUpPage;
