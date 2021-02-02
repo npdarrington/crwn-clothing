e7369efaec16d1fe882c7f11c3d8174536a1a433
Nathan Darrington <npdarrington@icloud.com>
2021-02-01 22:29:19 -0700

    Move CollectionsPreview into CollectionsOverview cmp

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,23 +1,11 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-import { selectCollections } from '../../redux/shop/shop.selector';
+const ShopPage = ({ collections }) => (
+	<section className='shop-page'>
+		<CollectionsOverview />
+	</section>
+);
 
-const ShopPage = ({ collections }) => {
-	return (
-		<section className='shop-page'>
-			{collections.map(({ id, ...otherCollectionProps }) => (
-				<CollectionPreview key={id} {...otherCollectionProps} />
-			))}
-		</section>
-	);
-};
-
-const mapStateToProps = createStructuredSelector({
-	collections: selectCollections,
-});
-
-export default connect(mapStateToProps)(ShopPage);
+export default ShopPage;
