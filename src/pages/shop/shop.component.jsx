b6290c557ed729966a8d4031bb60e79ecf512467
Nathan Darrington <npdarrington@icloud.com>
2021-02-01 22:13:11 -0700

    Change Shop component to functional component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,26 +1,15 @@
-import React, { Component } from 'react';
-
-import SHOP_DATA from '../../redux/shop/shop.data';
+import React from 'react';
 
 import CollectionPreview from '../../components/collection-preview/collection-preview.component';
 
-export default class ShopPage extends Component {
-	constructor(props) {
-		super(props);
+const ShopPage = ({ collections }) => {
+	return (
+		<section className='shop-page'>
+			{collections.map(({ id, ...otherCollectionProps }) => (
+				<CollectionPreview key={id} {...otherCollectionProps} />
+			))}
+		</section>
+	);
+};
 
-		this.state = {
-			collections: SHOP_DATA,
-		};
-	}
-
-	render() {
-		const { collections } = this.state;
-		return (
-			<section className='shop-page'>
-				{collections.map(({ id, ...otherCollectionProps }) => (
-					<CollectionPreview key={id} {...otherCollectionProps} />
-				))}
-			</section>
-		);
-	}
-}
+export default ShopPage;
