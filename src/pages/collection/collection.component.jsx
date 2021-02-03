3d9468b11d0e19167b84cb0ff9922d2413477baf
Nathan Darrington <npdarrington@icloud.com>
2021-02-02 22:56:47 -0700

    Create CollectionPage cmp

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import CollectionItem from '../../components/collection-item/collection-item.component';
+
+import './collection.styles.scss';
+
+const CollectionPage = ({ match }) => (
+	<section className='collection-page'>
+		<h2>Collection PAGE</h2>
+	</section>
+);
+
+export default CollectionPage;
