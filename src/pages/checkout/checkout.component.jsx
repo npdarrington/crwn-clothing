bd2b388fd31b1feaa8ea419e034021dada1deca7
Nathan Darrington <npdarrington@icloud.com>
2021-03-29 23:16:04 -0600

    Fix misspelling when passing prop

--- src/pages/checkout/checkout.component.jsx
+++ src/pages/checkout/checkout.component.jsx
@@ -37,7 +37,7 @@ const CheckoutPage = ({ cartItems, total }) => (
 		<article className='total'>
 			<span>TOTAL: ${total}</span>
 		</article>
-		<StripeCheckoutButton total={total} />
+		<StripeCheckoutButton price={total} />
 	</section>
 );
 
