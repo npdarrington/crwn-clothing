4b779e2c32afacf04ef531c4cfab39923131e81f
Nathan Darrington <npdarrington@icloud.com>
2021-04-06 11:47:53 -0600

    Create LogoContainer styles

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,10 +1,16 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
-const HeaderContainer = styled.section`
+export const HeaderContainer = styled.section`
   height: 70px;
   width: 100%;
   display: flex;
   justify-content: space-between;
   margin-bottom: 25px;
+`
+
+export const LogoContainer = styled(Link)`
+  height: 100%;
+  width: 70px;
+  padding: 25px;
 `
