3c9312b667c6e3eeca36edf760092bbdffed1145
Nathan Darrington <npdarrington@icloud.com>
2021-04-06 11:46:53 -0600

    Create HeaderContainer styles

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,2 +1,10 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
+const HeaderContainer = styled.section`
+  height: 70px;
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+`
