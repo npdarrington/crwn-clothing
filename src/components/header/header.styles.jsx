03c433bca81105526fd16301400f60869141844d
Nathan Darrington <npdarrington@icloud.com>
2021-04-06 12:07:10 -0600

    Created OptionsContainer styled cmp

--- src/components/header/header.styles.jsx
+++ src/components/header/header.styles.jsx
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { Link } from 'react-router-dom';
 
 export const HeaderContainer = styled.section`
@@ -14,3 +14,11 @@ export const LogoContainer = styled(Link)`
   width: 70px;
   padding: 25px;
 `
+
+export const OptionsContainer = styled.section`
+  width: 50%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
