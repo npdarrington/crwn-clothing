76ff0bc5e246a5679ba774817b404067ba2e316b
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 13:47:41 -0600

    Add background-image prop to ImageContainer

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -1,29 +1,34 @@
 import styled from 'styled-components';
 
+const renderImage = ({ imageUrl }) => {
+	return imageUrl;
+};
+
 export const CollectionItemContainer = styled.section`
-  width: 22vw;
-  display: flex;
-  flex-direction: column;
-  height: 350px;
-  align-items: center;
-  position: relative;
+	width: 22vw;
+	display: flex;
+	flex-direction: column;
+	height: 350px;
+	align-items: center;
+	position: relative;
 
-  button {
-    width: 80%;
-    opacity: 0.7;
-    position: absolute;
-    top: 255px;
+	button {
+		width: 80%;
+		opacity: 0.7;
+		position: absolute;
+		top: 255px;
 
-    &:hover {
-      opacity: 0.85;
-      display: flex;
-    }
-  }
+		&:hover {
+			opacity: 0.85;
+			display: flex;
+		}
+	}
 `;
 
-export const CollectionItemImage = styled.section`
+export const CollectionItemImageContainer = styled.section`
   width: 100%;
   height: 95%;
+  background-image: url(${renderImage});
   background-size: cover;
   background-position: center;
   margin-bottom: 5px;
