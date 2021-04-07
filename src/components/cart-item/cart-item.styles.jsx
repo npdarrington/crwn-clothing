4b597d5f2297a5eb8b3bc8f59d802eabe4fd437f
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 13:05:56 -0600

    Create CartItemContainer styled cmp

--- src/components/cart-item/cart-item.styles.jsx
+++ src/components/cart-item/cart-item.styles.jsx
@@ -1,2 +1,12 @@
 import styled from 'styled-components';
 
+export const CartItemContainer = styled.article`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  margin-bottom: 15px;
+
+  img {
+    width: 30%;
+  }
+`;
