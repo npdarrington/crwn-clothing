d888d2aad42f091d30fa453ea00917372f9a4fe4
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 16:56:36 -0600

    Created CollectionPreviewContainer

---
+++ src/components/collection-preview/collection-preview.styles.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const CollectionPreviewContainer = styled.section`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 30px;
+
+  h1 {
+    font-size: 28px;
+    margin-bottom: 25px;
+  }
+`;
