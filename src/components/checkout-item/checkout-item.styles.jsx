03fc9446683f1b3650e3fdb3ce026508ad24f96d
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 13:13:01 -0600

    Create CheckoutItemContainer styled cmp

--- src/components/checkout-item/checkout-item.styles.jsx
+++ src/components/checkout-item/checkout-item.styles.jsx
@@ -1,2 +1,11 @@
 import styled from 'styled-components';
 
+export const CheckoutItemContainer = styled.article`
+  width: 100%;
+	display: flex;
+	min-height: 100px;
+	border-bottom: 1px solid darkgrey;
+	padding: 15px 0;
+	font-size: 20px;
+	align-items: center;
+`;
