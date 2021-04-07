fc663f27b7f199ecbac323329ec247174a84f425
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 17:06:08 -0600

    Create DirectoryMenuContainer

--- src/components/directory/directory.styles.jsx
+++ src/components/directory/directory.styles.jsx
@@ -1,2 +1,8 @@
 import styled from 'styled-components';
 
+export const DirectoryMenuContainer = styled.section`
+  width: 100%;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+`;
