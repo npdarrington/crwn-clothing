9668817329bfcf7914f744a1aa3213a4e40c421b
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 11:12:11 -0600

    Create CartIconContainer styled cmp

--- src/components/cart-icon/cart-icon.styles.jsx
+++ src/components/cart-icon/cart-icon.styles.jsx
@@ -1,2 +1,11 @@
 import styled from 'styled-components';
 
+export const CartIconContainer = styled.article`
+  width: 45px;
+  height: 45px;
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  cursor: pointer;
+`;
