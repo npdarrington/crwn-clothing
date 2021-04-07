8b478892515bafa40194cd325078a7728ed767ec
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 13:28:55 -0600

    Created CollectionItemContainer cmp

--- src/components/collection-item/collection-item.styles.jsx
+++ src/components/collection-item/collection-item.styles.jsx
@@ -1,2 +1,10 @@
 import styled from 'styled-components';
 
+export const CollectionItemContainer = styled.section`
+  width: 22vw;
+  display: flex;
+  flex-direction: column;
+  height: 350px;
+  align-items: center;
+  position: relative;
+`;
