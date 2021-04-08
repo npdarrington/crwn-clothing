9cb08b2f81c67df0b9fb89d22ff089a332574aa1
Nathan Darrington <npdarrington@icloud.com>
2021-04-08 11:00:39 -0600

    Created BackgroundImageContainer

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,5 +1,9 @@
 import styled from 'styled-components';
 
+const displayBackgroundImage = ({ backgroundImage }) => {
+  return backgroundImage;
+}
+
 export const MenuItemContainer = styled.article`
   min-width: 30%;
   height: 240px;
@@ -13,5 +17,18 @@ export const MenuItemContainer = styled.article`
 
   &:hover {
     cursor: pointer;
+  }
+`;
+
+export const BackgroundImageContainer = styled.section`
+  width: 100%;
+  height: 100%;
+  background-position: center;
+  background-size: cover;
+  background-image: url(${displayBackgroundImage});
+
+  &:hover {
+    transform: scale(1.1);
+    transition: transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95);
   }
 `;
