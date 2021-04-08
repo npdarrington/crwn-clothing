17c89f6b1ba64ab8c39dffbecb974fefacf172a8
Nathan Darrington <npdarrington@icloud.com>
2021-04-08 10:54:44 -0600

    Created MenuItemContainer styled cmp

--- src/components/menu-item/menu-item.styles.jsx
+++ src/components/menu-item/menu-item.styles.jsx
@@ -1,2 +1,17 @@
 import styled from 'styled-components';
 
+export const MenuItemContainer = styled.article`
+  min-width: 30%;
+  height: 240px;
+  flex: 1 1 auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border: 1px solid black;
+  margin: 0 7.5px 15px;
+  overflow: hidden;
+
+  &:hover {
+    cursor: pointer;
+  }
+`;
