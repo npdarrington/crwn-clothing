27609628a8bf3ea47b8983e61545a3af06ff7337
Nathan Darrington <npdarrington@icloud.com>
2021-04-08 12:09:05 -0600

    Added h2 styling and created CollectionItemsContainer styled cmp

--- src/pages/collection/collection.styles.jsx
+++ src/pages/collection/collection.styles.jsx
@@ -3,4 +3,20 @@ import styled from 'styled-components';
 export const CollectionPageContainer = styled.section`
   display: flex;
   flex-direction: column;
+
+  h2 {
+    font-size: 38px;
+    margin: 0 auto 30px;
+  }
+`;
+
+export const CollectionItemsContainer = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-gap: 10px;
+  margin-bottom: 30px;
+
+  & > section {
+    margin-bottom: 30px;
+  }
 `;
