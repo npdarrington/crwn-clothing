2810080c62e25d5246d107ebe2be4caf808c88b5
Nathan Darrington <npdarrington@icloud.com>
2021-04-07 22:39:14 -0600

    Created FormInputContainer styled cmp

--- src/components/form-input/form-input.styles.jsx
+++ src/components/form-input/form-input.styles.jsx
@@ -1,6 +1,34 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+
+const shrinkLabel = css`
+  top: -14px;
+  font-size: 12px;
+  color: $main-color;
+`;
 
 export const FormGroupContainer = styled.section`
   position: relative;
   margin: 45px 0;
+`;
+
+export const FormInputContainer = styled.input`
+  background: none;
+  background-color: white;
+  color: grey;
+  font-size: 18px;
+  padding: 10px 10px 10px 5px;
+  display: block;
+  width: 100%;
+  border: none;
+  border-radius: 0;
+  border-bottom: 1px solid grey;
+  margin: 25px 0;
+
+  &:focus {
+    outline: none;
+  }
+
+  &:focus ~ label {
+    ${shrinkLabel}
+  }
 `;
