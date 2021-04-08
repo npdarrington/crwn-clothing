b1f8a436031a44edd33266a19a00567d32c9caa3
Nathan Darrington <npdarrington@icloud.com>
2021-04-08 11:41:54 -0600

    Created CheckoutPageContainer styled cmp

--- src/pages/checkout/checkout.styles.jsx
+++ src/pages/checkout/checkout.styles.jsx
@@ -1,2 +1,10 @@
 import styled from 'styled-components';
 
+export const CheckoutPageContainer = styled.section`
+  width: 55%;
+	min-height: 90vh;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	margin: 50px auto 0;
+`;
