3c862934986185cdcf1dac3c6208cac7abda8146
Nathan Darrington <npdarrington@icloud.com>
2021-04-08 11:37:39 -0600

    Added margin to h2

--- src/components/sign-in/sign-in.styles.jsx
+++ src/components/sign-in/sign-in.styles.jsx
@@ -4,6 +4,10 @@ export const SignInContainer = styled.article`
   width: 380px;
 	display: flex;
 	flex-direction: column;
+
+  h2 {
+    margin: 10px 0;
+  }
 `;
 
 export const SignInButtonContainer = styled.section`
