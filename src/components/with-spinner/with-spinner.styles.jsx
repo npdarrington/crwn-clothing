4fe0349d3c08bd32b8ef66bda1373b6053d71af6
Nathan Darrington <npdarrington@icloud.com>
2021-04-09 15:02:18 -0600

    Create SpinnerOverlay styled cmp

---
+++ src/components/with-spinner/with-spinner.styles.jsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const SpinnerOverlay = styled.section`
+  height: 60vh;
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
