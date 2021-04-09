2f5cc0239b5775f056a872e1c891b02876f172fd
Nathan Darrington <npdarrington@icloud.com>
2021-04-09 15:04:29 -0600

    Create SpinnerContainer styled cmp

--- src/components/with-spinner/with-spinner.styles.jsx
+++ src/components/with-spinner/with-spinner.styles.jsx
@@ -7,3 +7,23 @@ export const SpinnerOverlay = styled.section`
   justify-content: center;
   align-items: center;
 `;
+
+export const SpinnerContainer = styled.div`
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  border: 3px solid rgba(195, 195, 195, 0.6);
+  border-radius: 50%;
+  border-top-color: #636767;
+  animation: spin 1s ease-in-out infinite;
+  -webkit-anmimation: spin 1s ease-in-out infinite;
+
+  @keyframes spin {
+    to {
+      -webkit-transform: rotate(360deg);
+    }
+  }
+  @-website-keyframes spin {
+    -webkit-transform: rotate(360deg);
+  }
+`;
