5b56f102f839bc338bd12c9a59f31e59a592331c
Nathan Darrington <npdarrington@icloud.com>
2021-04-09 12:15:07 -0600

    Remove code to add shop data to firestore collection

--- src/App.js
+++ src/App.js
@@ -12,17 +12,16 @@ import CheckoutPage from './pages/checkout/checkout.component';
 
 import Header from './components/header/header.component';
 
-import { auth, createUserProfileDocument, addCollectionAndDocuments } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 import { setCurrentUser } from './redux/user/user.actions';
 import { selectCurrentUser } from './redux/user/user.selectors';
-import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends Component {
 	unsubscribeFromAuth = null;
 
 	componentDidMount() {
-		const { setCurrentUser, collectionsArray } = this.props;
+		const { setCurrentUser } = this.props;
 		this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
 			if (userAuth) {
 				const userRef = await createUserProfileDocument(userAuth);
@@ -35,10 +34,9 @@ class App extends Component {
 						},
 					});
 				});
+			} else {
+				setCurrentUser(userAuth);
 			}
-				
-			setCurrentUser(userAuth);
-			addCollectionAndDocuments('collections', collectionsArray.map(({ title, items }) => ({ title, items })));
 		});
 	}
 
@@ -73,7 +71,6 @@ class App extends Component {
 
 const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser,
-	collectionsArray: selectCollectionsForPreview
 });
 
 const mapDispatchToProps = dispatch => ({
