777d9ca0b9635200d098d1e82385ac0a522dece6
Nathan Darrington <npdarrington@icloud.com>
2021-04-09 14:35:04 -0600

    Create convertCollectionsSnapShotToMap method

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -45,6 +45,18 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 	return await batch.commit();
 }
 
+export const convertCollectionsSnapshotToMap = (collections) => {
+	const transformedCollections = collections.docs.map(doc => {
+		const { title, items } = doc.data();
+		return {
+			routeName: encodeURI(title.toLowerCase()),
+			id: doc.id,
+			title,
+			items
+		}
+	})
+}
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
