c056d15b7aca6a92304e7d670ac22100e5e9cafc
Nathan Darrington <npdarrington@icloud.com>
2021-04-14 12:50:53 -0600

    Initial CollectionContainer HOC

---
+++ src/pages/collection/collection.container.jsx
@@ -0,0 +1,4 @@
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { createStructuredSelector } from 'reselect';
+import { selectIsCollectionsLoaded } from '../../redux/shop/shop.selector';
