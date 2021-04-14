4a452b799aab4733e54831d60721fd8029250ed0
Nathan Darrington <npdarrington@icloud.com>
2021-04-14 10:48:55 -0600

    Change to fetchCollectionsStart that starts the shopActionType without payload

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,6 @@
 import ShopActionTypes from './shop.types';
 
-export const updateCollections = (collectionsMap) => ({
-  type: ShopActionTypes.UPDATE_COLLECTIONS,
-  payload: collectionsMap
-})
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START
+});
+
