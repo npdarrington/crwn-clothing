c849530bf87e6b8ac9fbe98c48151e24d5f05782
Nathan Darrington <npdarrington@icloud.com>
2021-04-14 11:02:40 -0600

    create selectIsCollectionFetching

--- src/redux/shop/shop.selector.js
+++ src/redux/shop/shop.selector.js
@@ -22,3 +22,8 @@ export const selectCollection = memoize(collectionUrlParam =>
 			collections[collectionUrlParam] : null
 	)
 );
+
+export const selectIsCollectionFetching = createSelector(
+	[selectShop],
+	shop => shop.isFetching
+)
