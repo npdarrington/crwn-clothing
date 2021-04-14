45ac5c98dd17426c634c635caa75c9fda0c45998
Nathan Darrington <npdarrington@icloud.com>
2021-04-14 10:52:30 -0600

    Create fetchCollectionsStartAysnc that fires fetchCollectionsStart

--- src/redux/shop/shop.actions.js
+++ src/redux/shop/shop.actions.js
@@ -1,6 +1,19 @@
 import ShopActionTypes from './shop.types';
 
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+
 export const fetchCollectionsStart = () => ({
   type: ShopActionTypes.FETCH_COLLECTIONS_START
 });
 
+export const fetchCollectionsStartAsync = () => {
+  return dispatch => {
+    const collectionRef = firestore.collection('collections');
+    dispatch(fetchCollectionsStart());
+		
+		collectionRef.get().then(snapshot => {
+			const collectionsMap = convertCollectionsSnapshotToMap(snapshot);
+			this.setState({ loading: false });
+		});
+  }
+}
