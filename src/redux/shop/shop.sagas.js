c4583da411205d14515199617b3b9c4249a62c03
Nathan Darrington <npdarrington@icloud.com>
2021-04-17 17:56:54 -0600

    Create shopSaga for consistency with rootSaga

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest, call, put } from 'redux-saga/effects';
+import { takeLatest, call, put, all } from 'redux-saga/effects';
 import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils';
 
 import { 
@@ -24,4 +24,8 @@ export function* fetchCollectionsStart() {
 		ShopActionTypes.FETCH_COLLECTIONS_START,
 		fetchCollectionsAsync
 	);
+}
+
+export function* shopSaga() {
+	yield all([call(fetchCollectionsAsync)]);
 }
