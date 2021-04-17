ef8057f5fd7082ee2563e4d163537beb648d849f
Nathan Darrington <npdarrington@icloud.com>
2021-04-16 20:47:54 -0600

    Refactor and make getSnapshotFromUserAuth

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -10,10 +10,9 @@ import {
 	createUserProfileDocument,
 } from '../../firebase/firebase.utils';
 
-export function* signInWithGoogle() {
+export function* getSnapshotFromUserAuth(userAuth) {
 	try {
-		const { user } = yield auth.signInWithPopup(googleProvider);
-		const userRef = yield call(createUserProfileDocument, user);
+		const userRef = yield call(createUserProfileDocument, userAuth);
 		const userSnapshot = yield userRef.get();
 		yield put(
 			signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
@@ -23,14 +22,19 @@ export function* signInWithGoogle() {
 	}
 }
 
+export function* signInWithGoogle() {
+	try {
+		const { user } = yield auth.signInWithPopup(googleProvider);
+		yield getSnapshotFromUserAuth(user);
+	} catch (error) {
+		yield put(signInFailure(error.message));
+	}
+}
+
 export function* signInWithEmail({ payload: { email, password } }) {
 	try {
 		const { user } = yield auth.signInWithEmailAndPassword(email, password);
-		const userRef = yield call(createUserProfileDocument, user);
-		const userSnapshot = yield userRef.get();
-		yield put(
-			signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
-		);
+		yield getSnapshotFromUserAuth(user);
 	} catch (error) {
 		put(signInFailure(error.message));
 	}
