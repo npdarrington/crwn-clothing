eb2a5529ac6c790a11d31ce81b0be3941c777c14
Nathan Darrington <npdarrington@icloud.com>
2021-04-16 20:41:56 -0600

    Update to use signInSuccess and signInFailure

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -2,12 +2,7 @@ import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import {
-	googleSignInSuccess,
-	googleSignInFailure,
-	emailSignInSuccess,
-	emailSignInFailure,
-} from './user.actions';
+import { signInSuccess, signInFailure } from './user.actions';
 
 import {
 	auth,
@@ -21,23 +16,23 @@ export function* signInWithGoogle() {
 		const userRef = yield call(createUserProfileDocument, user);
 		const userSnapshot = yield userRef.get();
 		yield put(
-			googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+			signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
 		);
 	} catch (error) {
-		yield put(googleSignInFailure(error.message));
+		yield put(signInFailure(error.message));
 	}
 }
 
-export function* signInWithEmail({ payload : { email, password } }) {
+export function* signInWithEmail({ payload: { email, password } }) {
 	try {
 		const { user } = yield auth.signInWithEmailAndPassword(email, password);
 		const userRef = yield call(createUserProfileDocument, user);
 		const userSnapshot = yield userRef.get();
 		yield put(
-			emailSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+			signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
 		);
 	} catch (error) {
-		put(emailSignInFailure(error.message));
+		put(signInFailure(error.message));
 	}
 }
 
