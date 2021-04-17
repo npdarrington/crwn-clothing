769893cf174d349537fdcff63917049e83a61611
Nathan Darrington <npdarrington@icloud.com>
2021-04-17 17:23:00 -0600

    On componentDidMount checkUserSession

--- src/App.js
+++ src/App.js
@@ -13,9 +13,15 @@ import CheckoutPage from './pages/checkout/checkout.component';
 import Header from './components/header/header.component';
 
 import { selectCurrentUser } from './redux/user/user.selectors';
+import { checkUserSession } from './redux/user/user.actions';
 
 class App extends Component {
 	unsubscribeFromAuth = null;
+
+	componentDidMount() {
+		const { checkUserSession } = this.props;
+		checkUserSession();
+	}
 
 	componentWillUnmount() {
 		this.unsubscribeFromAuth();
@@ -50,4 +56,8 @@ const mapStateToProps = createStructuredSelector({
 	currentUser: selectCurrentUser,
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+	checkUserSession: () => dispatch(checkUserSession())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
