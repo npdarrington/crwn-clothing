5095d08f4c9a6b390949beaf8110cc342a05161c
Nathan Darrington <npdarrington@icloud.com>
2021-04-16 19:59:18 -0600

    Finish creating signInWithGoogle saga and export userSagas

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -1,19 +1,32 @@
-import { takeLatest, put } from 'redux-saga/effects';
+import { takeLatest, put, all, call } from 'redux-saga/effects';
 
 import UserActionTypes from './user.types';
 
-import { auth, googleProvider, createUserProfileDocument } from '../../firebase/firebase.utils';
+import { googleSignInSuccess, googleSignInFailure } from './user.actions';
+
+import {
+	auth,
+	googleProvider,
+	createUserProfileDocument,
+} from '../../firebase/firebase.utils';
 
 export function* signInWithGoogle() {
-  try {
-    const userRef = yield auth.signInWithPopup(googleProvider);
-    console.log(userRef);
-  } catch (error) {
-    
-  }
+	try {
+		const { user } = yield auth.signInWithPopup(googleProvider);
+		const userRef = yield call(createUserProfileDocument, user);
+		const userSnapshot = yield userRef.get();
+		yield put(
+			googleSignInSuccess({ id: userSnapshot.id, ...userSnapshot.data() })
+		);
+	} catch (error) {
+		yield put(googleSignInFailure(error.message));
+	}
 }
 
 export function* onGoogleSignInStart() {
-  yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, );
+	yield takeLatest(UserActionTypes.GOOGLE_SIGN_IN_START, signInWithGoogle);
 }
 
+export function* userSagas() {
+	yield all([call(onGoogleSignInStart)]);
+}
