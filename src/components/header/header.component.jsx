2a70b5f22c9356c324a2f5e698706206ecac8c58
Nathan Darrington <npdarrington@icloud.com>
2021-04-17 17:43:11 -0600

    Import signOutStart and invooke when SIGN OUT is clicked, remove unused file refs

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { signOutStart } from '../../redux/user/user.actions';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -17,7 +17,7 @@ import {
 	OptionLink,
 } from './header.styles';
 
-const Header = ({ currentUser, hidden }) => (
+const Header = ({ currentUser, hidden, signOutStart }) => (
 	<HeaderContainer>
 		<LogoContainer to='/'>
 			<Logo />
@@ -26,7 +26,7 @@ const Header = ({ currentUser, hidden }) => (
 			<OptionLink to='/shop'>SHOP</OptionLink>
 			<OptionLink to='/shop'>CONTACT</OptionLink>
 			{currentUser ? (
-				<OptionLink as='section' onClick={() => auth.signOut()}>SIGN OUT</OptionLink>
+				<OptionLink as='section' onClick={signOutStart}>SIGN OUT</OptionLink>
 			) : (
 				<OptionLink to='/signin'>SIGN IN</OptionLink>
 			)}
@@ -41,4 +41,8 @@ const mapStateToProps = createStructuredSelector({
 	hidden: selectCartHidden,
 });
 
-export default connect(mapStateToProps)(Header);
+const mapDispatchToProps = dispatch => ({
+	signOutStart: () => dispatch(signOutStart())
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Header);
