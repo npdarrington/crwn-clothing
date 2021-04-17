24a78eecbc20f093e040f9645693a02243a75a3a
Nathan Darrington <npdarrington@icloud.com>
2021-04-17 17:22:41 -0600

    Create getCurrentUser to return a Promise obj, format file

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -25,17 +25,20 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
 				displayName,
 				email,
 				createdAt,
-				...additionalData
-			})
+				...additionalData,
+			});
 		} catch (error) {
 			console.log('error creating user', error.message);
 		}
 	}
 
 	return userRef;
-}
+};
 
-export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+export const addCollectionAndDocuments = async (
+	collectionKey,
+	objectsToAdd
+) => {
 	const collectionRef = firestore.collection(collectionKey);
 	const batch = firestore.batch();
 	objectsToAdd.forEach(obj => {
@@ -43,26 +46,35 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
 		batch.set(newDocRef, obj);
 	});
 	return await batch.commit();
-}
+};
 
-export const convertCollectionsSnapshotToMap = (collections) => {
+export const convertCollectionsSnapshotToMap = collections => {
 	const transformedCollections = collections.docs.map(doc => {
 		const { title, items } = doc.data();
 		return {
 			routeName: encodeURI(title.toLowerCase()),
 			id: doc.id,
 			title,
-			items
-		}
+			items,
+		};
 	});
 
 	return transformedCollections.reduce((acc, collection) => {
 		acc[collection.title.toLowerCase()] = collection;
 		return acc;
 	}, {});
-}
+};
 
 firebase.initializeApp(config);
+
+export const getCurrentUser = () => {
+	return new Promise((resolve, reject) => {
+		const unsubscribe = auth.onAuthStateChanged(userAuth => {
+			unsubscribe();
+			resolve(userAuth);
+		}, reject);
+	});
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
