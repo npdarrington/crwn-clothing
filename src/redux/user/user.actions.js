f94f0bcf27bb3065fc7ae8500ad74a08c4fedec3
Nathan Darrington <npdarrington@icloud.com>
2021-04-16 19:55:42 -0600

    Fix bug with googleSignInSuccess using GOOGLE_SIGN_IN_START

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -1,4 +1,4 @@
-import { UserActionTypes } from './user.types'
+import UserActionTypes from './user.types'
 
 export const setCurrentUser = user => ({
 	type: UserActionTypes.SET_CURRENT_USER,
@@ -10,7 +10,7 @@ export const googleSignInStart = () => ({
 });
 
 export const googleSignInSuccess = user => ({
-	type: UserActionTypes.GOOGLE_SIGN_IN_START,
+	type: UserActionTypes.GOOGLE_SIGN_IN_SUCCESS,
 	payload: user
 });
 
