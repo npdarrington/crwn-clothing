1739ebb5efc2938120407a42f212fdfc0305ee03
Nathan Darrington <npdarrington@icloud.com>
2021-04-18 17:29:14 -0600

    Refactor shop cmp to use hooks

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
@@ -7,32 +7,28 @@ import CollectionPageContainer from '../collection/collection.container';
 
 import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 
-class ShopPage extends React.Component {
-	componentDidMount() {
-		const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+	useEffect(() => {
 		fetchCollectionsStart();
-	}
+	}, [fetchCollectionsStart]);
 
-	render() {
-		const { match } = this.props;
-		return (
-			<section>
-				<Route
-					exact
-					path={`${match.path}`}
-					component={CollectionsOverviewContainer}
-				/>
-				<Route
-					path={`${match.path}/:collectionId`}
-					component={CollectionPageContainer}
-				/>
-			</section>
-		);
-	}
-}
+	return (
+		<section>
+			<Route
+				exact
+				path={`${match.path}`}
+				component={CollectionsOverviewContainer}
+			/>
+			<Route
+				path={`${match.path}/:collectionId`}
+				component={CollectionPageContainer}
+			/>
+		</section>
+	);
+};
 
 const mapDispatchToProps = dispatch => ({
-	fetchCollectionsStart: () => dispatch(fetchCollectionsStart())
+	fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
 });
 
 export default connect(null, mapDispatchToProps)(ShopPage);
