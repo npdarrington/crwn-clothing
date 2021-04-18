bdf4da6fdb4b099db550ca1b6129640f7427ea84
Nathan Darrington <npdarrington@icloud.com>
2021-04-18 16:04:19 -0600

    Refactor registerUser, change to signUp and add signInAfterSignUp saga

--- src/redux/user/user.sagas.js
+++ src/redux/user/user.sagas.js
@@ -20,7 +20,11 @@ import {
 
 export function* getSnapshotFromUserAuth(userAuth, additionalData) {
 	try {
-		const userRef = yield call(createUserProfileDocument, userAuth, additionalData);
+		const userRef = yield call(
+			createUserProfileDocument,
+			userAuth,
+			additionalData
+		);
 		const userSnapshot = yield userRef.get();
 		yield put(signInSuccess({ id: userSnapshot.id, ...userSnapshot.data() }));
 	} catch (error) {
@@ -65,14 +69,17 @@ export function* signOut() {
 	}
 }
 
-export function* registerUser({ payload : { displayName, email, password } }) {
+export function* signUp({ payload: { displayName, email, password } }) {
 	try {
 		const { user } = yield auth.createUserWithEmailAndPassword(email, password);
-		yield put(signUpSuccess());
-		yield getSnapshotFromUserAuth(user, { displayName });
+		yield put(signUpSuccess({ user, additionalData: { displayName } }));
 	} catch (error) {
 		yield put(signUpFailure(error));
 	}
+}
+
+export function* signInAfterSignUp({ payload: { user, additionalData } }) {
+	yield getSnapshotFromUserAuth(user, additionalData);
 }
 
 export function* onGoogleSignInStart() {
@@ -92,7 +99,11 @@ export function* onSignOutStart() {
 }
 
 export function* onSignUpStart() {
-	yield takeLatest(UserActionTypes.SIGN_UP_START, registerUser);
+	yield takeLatest(UserActionTypes.SIGN_UP_START, signUp);
+}
+
+export function* onSignUpSuccess() {
+	yield takeLatest(UserActionTypes.SIGN_UP_SUCCESS, signInAfterSignUp);
 }
 
 export function* userSagas() {
@@ -101,6 +112,7 @@ export function* userSagas() {
 		call(onEmailSignInStart),
 		call(onCheckUserSession),
 		call(onSignOutStart),
-		call(onSignUpStart)
+		call(onSignUpStart),
+		call(onSignUpSuccess),
 	]);
 }
