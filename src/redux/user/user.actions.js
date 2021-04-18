7b50e55e1098f9762dd99d2c8e833fdf9c366813
Nathan Darrington <npdarrington@icloud.com>
2021-04-18 12:59:08 -0600

    Add sign up actions

--- src/redux/user/user.actions.js
+++ src/redux/user/user.actions.js
@@ -35,3 +35,17 @@ export const signOutFailure = error => ({
 	type: UserActionTypes.SIGN_OUT_FAILURE,
 	payload: error
 });
+
+export const signUpStart = userInfo => ({
+	type: UserActionTypes.SIGN_UP_START,
+	payload: userInfo
+});
+
+export const signUpSuccess = () => ({
+	type: UserActionTypes.SIGN_UP_SUCCESS
+});
+
+export const signUpFailure = error => ({
+	type: UserActionTypes.SIGN_UP_FAILURE,
+	payload: error
+});
