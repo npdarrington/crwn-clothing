3d39c7b79feb7c8663f34c1d27e32f626cce935a
Nathan Darrington <npdarrington@icloud.com>
2021-04-21 22:36:27 -0600

    Adjust styling for tablet and mobile view

--- src/pages/collection/collection.styles.jsx
+++ src/pages/collection/collection.styles.jsx
@@ -16,6 +16,18 @@ export const CollectionItemsContainer = styled.section`
   grid-gap: 10px;
   margin-bottom: 30px;
 
+  @media screen and (max-width: 950px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media screen and (max-width: 700px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+
+  @media screen and (max-width: 500px) {
+    grid-template-columns: 1fr;
+  }
+
   & > section {
     margin-bottom: 30px;
   }
