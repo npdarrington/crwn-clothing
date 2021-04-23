48d35e1a8897e52aaf0de1f2ec5352c0e00b5d31
Nathan Darrington <npdarrington@icloud.com>
2021-04-22 22:51:38 -0600

    Reduce padding for tablet and mobile devices

--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -5,4 +5,8 @@ export const HomePageContainer = styled.section`
   flex-direction: column;
   align-items: center;
   padding: 20px 80px;
+
+  @media screen and (max-width: 768px) {
+    padding: 20px;
+  }
 `;
