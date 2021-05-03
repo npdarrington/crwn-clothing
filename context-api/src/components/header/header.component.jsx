f0bd254c53828fa9d0bacabdbb3ebb209727a288
Nathan Darrington <npdarrington@icloud.com>
2021-05-02 22:11:32 -0600

    Replace context refs and code with new provider code

--- context-api/src/components/header/header.component.jsx
+++ context-api/src/components/header/header.component.jsx
@@ -8,7 +8,7 @@ import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import CurrentUserContext from '../../contexts/current-user/current-user.context';
-import CartContext from '../../contexts/cart/cart.context';
+import { CartContext } from '../../providers/cart/cart.provider';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 
@@ -16,8 +16,7 @@ import './header.styles.scss';
 
 const Header = () => {
 	const currentUser = useContext(CurrentUserContext);
-	const [hidden, setHidden] = useState(true);
-	const toggleHidden = () => setHidden(!hidden);
+	const { hidden } = useContext(CartContext);
 
 	return (
 		<div className='header'>
@@ -40,13 +39,7 @@ const Header = () => {
 						SIGN IN
 					</Link>
 				)}
-				<CartContext.Provider
-					value={{
-						hidden,
-						toggleHidden,
-					}}>
-					<CartIcon />
-				</CartContext.Provider>
+				<CartIcon />
 			</div>
 			{hidden ? null : <CartDropdown />}
 		</div>
