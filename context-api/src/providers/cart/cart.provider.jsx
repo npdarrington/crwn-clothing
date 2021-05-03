009cc0f6676f9ca1d9c1e25a3064a2373c70e525
Nathan Darrington <npdarrington@icloud.com>
2021-05-03 10:22:48 -0600

    Set CartContext prop total with useEffect, format file

--- context-api/src/providers/cart/cart.provider.jsx
+++ context-api/src/providers/cart/cart.provider.jsx
@@ -1,42 +1,56 @@
 import React, { createContext, useState, useEffect } from 'react';
 
-import { addItemToCart, removeItemFromCart, filterItemFromCart, getCartItemsCount } from './cart.utils';
+import {
+	addItemToCart,
+	removeItemFromCart,
+	filterItemFromCart,
+	getCartItemsCount,
+	getTotalPriceInCart,
+} from './cart.utils';
 
 export const CartContext = createContext({
 	hidden: true,
 	toggleHidden: () => {},
-  cartItems: [],
-  addItem: () => {},
-  removeItem: () => {},
-  clearItemFromCart: () => {},
-  cartItemsCount: 0
+	cartItems: [],
+	addItem: () => {},
+	removeItem: () => {},
+	clearItemFromCart: () => {},
+	cartItemsCount: 0,
+	total: 0,
 });
 
 const CartProvider = ({ children }) => {
-  const [hidden, setHidden] = useState(true);
-  const [cartItems, setCartItems] = useState([]);
-  const [cartItemsCount, setCartItemsCount] = useState(0);
+	const [hidden, setHidden] = useState(true);
+	const [cartItems, setCartItems] = useState([]);
+	const [cartItemsCount, setCartItemsCount] = useState(0);
+	const [total, setTotal] = useState(0);
 
-  const addItem = item => setCartItems(addItemToCart(cartItems, item));
-  const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
-  const clearItemFromCart = item => setCartItems(filterItemFromCart(cartItems, item));
+	const addItem = item => setCartItems(addItemToCart(cartItems, item));
+	const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
+	const clearItemFromCart = item =>
+		setCartItems(filterItemFromCart(cartItems, item));
 	const toggleHidden = () => setHidden(!hidden);
 
-  useEffect(() => {
-    setCartItemsCount(getCartItemsCount(cartItems));
-  }, [cartItems])
+	useEffect(() => {
+		setCartItemsCount(getCartItemsCount(cartItems));
+		setTotal(getTotalPriceInCart(cartItems));
+	}, [cartItems]);
 
-  return (
-    <CartContext.Provider value={{
-      hidden,
-      toggleHidden,
-      cartItems,
-      addItem,
-      cartItemsCount,
-      removeItem,
-      clearItemFromCart
-    }}>{children}</CartContext.Provider>
-  )
-}
+	return (
+		<CartContext.Provider
+			value={{
+				hidden,
+				toggleHidden,
+				cartItems,
+				addItem,
+				cartItemsCount,
+				removeItem,
+				clearItemFromCart,
+				total,
+			}}>
+			{children}
+		</CartContext.Provider>
+	);
+};
 
 export default CartProvider;
