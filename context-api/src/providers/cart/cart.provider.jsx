264851ce9803dd3c6e4ad60d229ca9d6c35d828d
Nathan Darrington <npdarrington@icloud.com>
2021-05-03 09:24:53 -0600

    Add removeItem to cart provider

--- context-api/src/providers/cart/cart.provider.jsx
+++ context-api/src/providers/cart/cart.provider.jsx
@@ -18,6 +18,7 @@ const CartProvider = ({ children }) => {
   const [cartItemsCount, setCartItemsCount] = useState(0);
 
   const addItem = item => setCartItems(addItemToCart(cartItems, item));
+  const removeItem = item => setCartItems(removeItemFromCart(cartItems, item));
 	const toggleHidden = () => setHidden(!hidden);
 
   return (
@@ -26,7 +27,8 @@ const CartProvider = ({ children }) => {
       toggleHidden,
       cartItems,
       addItem,
-      cartItemsCount
+      cartItemsCount,
+      removeItem
     }}>{children}</CartContext.Provider>
   )
 }
