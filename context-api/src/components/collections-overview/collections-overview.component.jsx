1e7c9e25a427f58e3ac0ebea1ddc63183ef87ed3
Nathan Darrington <npdarrington@icloud.com>
2021-05-08 14:47:09 -0600

    Remove Redux, use CollectionsContext

--- context-api/src/components/collections-overview/collections-overview.component.jsx
+++ context-api/src/components/collections-overview/collections-overview.component.jsx
@@ -1,23 +1,20 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React, { useContext } from 'react';
 
 import CollectionPreview from '../collection-preview/collection-preview.component';
 
-import { selectCollectionsForPreview } from '../../redux/shop/shop.selectors';
+import CollectionsContext from '../../contexts/collections/collections.context';
 
 import './collections-overview.styles.scss';
 
-const CollectionsOverview = ({ collections }) => (
+const CollectionsOverview = () => {
+  const allCollections = useContext(CollectionsContext);
+  const collections = Object.keys(allCollections).map(collection => allCollections[collection])
+  return (
   <div className='collections-overview'>
     {collections.map(({ id, ...otherCollectionProps }) => (
       <CollectionPreview key={id} {...otherCollectionProps} />
     ))}
   </div>
-);
+)};
 
-const mapStateToProps = createStructuredSelector({
-  collections: selectCollectionsForPreview
-});
-
-export default connect(mapStateToProps)(CollectionsOverview);
+export default CollectionsOverview;
